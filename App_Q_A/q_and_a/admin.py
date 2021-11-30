ddb56a457a0862467720546ec92da42408c6aa93
seraf7 <zaske_4@hotmail.com>
2021-11-29 22:23:06 -0600

    Modelado de preguntas y respuestas, integración con sitio de administración

--- App_Q_A/q_and_a/admin.py
+++ App_Q_A/q_and_a/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Pregunta, Respuesta
 
-# Register your models here.
+# Clase para definir detalles vistos en el sitio de administración
+class PreguntaAdmin(admin.ModelAdmin):
+    list_display = ('autor', 'fecha_publicacion', 'titulo')
+    search_fields = ['titulo', 'detalle']
+
+class RespuestaAdmin(admin.ModelAdmin):
+    list_display = ('pregunta', 'fecha_respuesta', 'contenido')
+    list_filter = ('status', )
+
+# Se añaden los modelos al sitio de administración
+admin.site.register(Pregunta, PreguntaAdmin)
+admin.site.register(Respuesta, RespuestaAdmin)
--- App_Q_A/q_and_a/models.py
+++ App_Q_A/q_and_a/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Clase para modelar una pregunta
+class Pregunta(models.Model):
+    titulo = models.CharField(max_length=200)
+    detalle = models.TextField()
+    fecha_publicacion = models.DateTimeField()
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Clase para ordenar preguntas por la más reciente
+    class Meta:
+        ordering = ['-fecha_publicacion']
+
+    # Representa objeto como cadena
+    def __str__(self):
+        return self.titulo
+
+CORRECTA = ((0, "Respuesta sin validar"), (1, "Respuesta correcta"))
+
+# Clase para modelar una respuesta
+class Respuesta(models.Model):
+    contenido = models.TextField()
+    fecha_respuesta = models.DateTimeField()
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=CORRECTA, default=0)
+
+    # Representación como cadena
+    def __str__(self):
+        return self.contenido
